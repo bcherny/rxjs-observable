90ab11bea7eda585a8d67b5f96a9e4f384271146
Boris Cherny <boris@performancejs.com>
2018-09-07 20:33:24 -0700

    interoperate with rx5 observable symbol

--- src/symbol/observable.ts
+++ src/symbol/observable.ts
@@ -1,12 +1,27 @@
-/** Symbol.observable addition */
-/* Note: This will add Symbol.observable globally for all TypeScript users,
-  however, we are no longer polyfilling Symbol.observable */
 declare global {
   interface SymbolConstructor {
     readonly observable: symbol
   }
 }
 
+// Lifted from https://github.com/ReactiveX/rxjs/blob/5.3.0/src/symbol/observable.ts
 /** Symbol.observable or a string "@@observable". Used for interop */
-export const observable =
-  (typeof Symbol === 'function' && Symbol.observable) || '@@observable'
+export function getSymbolObservable(context: any) {
+  let $$observable: any
+  let Symbol = context.Symbol
+
+  if (typeof Symbol === 'function') {
+    if (Symbol.observable) {
+      $$observable = Symbol.observable
+    } else {
+      $$observable = Symbol('observable')
+      Symbol.observable = $$observable
+    }
+  } else {
+    $$observable = '@@observable'
+  }
+
+  return $$observable
+}
+
+export const observable = getSymbolObservable(typeof window === 'undefined' ? {} : window)
